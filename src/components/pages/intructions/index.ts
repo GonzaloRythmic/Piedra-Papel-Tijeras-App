cdb44ff4b3bd929a61243cb50db589f4fa87ed52
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2022-03-24 19:38:41 -0300

    24/3/2022 19:38

--- src/components/pages/intructions/index.ts
+++ src/components/pages/intructions/index.ts
@@ -1,50 +1,13 @@
 export function initInstructions(param){
-    const div = document.createElement("div");
-    div.className = "container";
-
-    const style = document.createElement("style");
-
-    style.innerHTML =`
-      .home-title-container{
-        width: 100%;
-        height: auto;
-        text-align: center;
-        display: flex;
-        justify-content: center;
-      }
-      .home-title{
-        font-family: "Permanent Marker";
-        font-size: 40px;
-      }
-      .home-button-container{
-        width: 100%;
-        height: auto;
-        display: flex;
-        justify-content: center;
-        margin-top: 75px;
-      }
-      .home-button{
-        background-color: black;
-      }
-      .img-container{
-        width: 100%;
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        column-gap: 50px;
-        position: absolute; bottom: 0;
-      }
-      .img{
-        width: 56px;
-        height: 128px;
-      }
-    `
-
-    const rock = require("url:../../../../images/piedra.png")
-    const sisors = require("url:../../../../images/tijera.png")
-    const paper = require("url:../../../../images/papel.png")
-    const playButton = require("url:../../../../images/jugarBoton.png")
+  const div = document.createElement("div");
+  div.className = "container";
+    
+  const rock = require("url:../../../../images/piedra.png")
+  const sisors = require("url:../../../../images/tijera.png")
+  const paper = require("url:../../../../images/papel.png")
+  const playButton = require("url:../../../../images/jugarBoton.png")
   
-    div.innerHTML = `
+  div.innerHTML = `
       <div class = home-title-container>
         <h2 class = home-title> 
           Presioná jugar
@@ -57,11 +20,48 @@ export function initInstructions(param){
         <img class = img src="${sisors}">
         <img class = img src="${paper}">
       </div>
-      `;
+  `;
 
-    div.appendChild(style);
+  const style = document.createElement("style");
+
+  style.innerHTML =`
+        .home-title-container{
+          width: 100%;
+          height: auto;
+          text-align: center;
+          display: flex;
+          justify-content: center;
+        }
+        .home-title{
+          font-family: "Permanent Marker";
+          font-size: 40px;
+        }
+        .home-button-container{
+          width: 100%;
+          height: auto;
+          display: flex;
+          justify-content: center;
+          margin-top: 75px;
+        }
+        .home-button{
+          background-color: black;
+        }
+        .img-container{
+          width: 100%;
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;
+          column-gap: 50px;
+          position: absolute; bottom: 0;
+        }
+        .img{
+          width: 56px;
+          height: 128px;
+        }
+  `
+
+  div.appendChild(style);
   
-    div.querySelector(".home-button").addEventListener("click", () => param.goTo("/game"));
+  div.querySelector(".home-button").addEventListener("click", () => param.goTo("/game"));
   
-    return div;
+  return div;
 }
