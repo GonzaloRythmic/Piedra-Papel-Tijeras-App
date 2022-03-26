88bf3e0b45c69b7329b97b7dc0f884b8d72b0a0c
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2022-03-26 15:27:40 -0300

    26/3/2022 15:27

--- src/components/pages/game/index.ts
+++ src/components/pages/game/index.ts
@@ -4,8 +4,61 @@ export function initPlay(param){
     const div = document.createElement("div");
     div.className = "container";
 
-    const style = document.createElement("style");
+    const rock = require("url:../../../../images/piedra.png")
+    const sisors = require("url:../../../../images/tijera.png")
+    const paper = require("url:../../../../images/papel.png")
 
+    div.innerHTML = `
+    <div class = countdown-container>
+        <div class = countdown id="countdown"></div>
+    </div>
+
+    <div class= img-container>
+        <div play = "rock">
+            <div class = zoom><img class = "rock" src="${rock}" /></div>
+        </div>
+        <div play = "scissors">
+            <div class = zoom><img class = "sisors" src="${sisors}" /></div>
+        </div>
+        <div play = "paper">
+            <div class = zoom><img class = "paper" src="${paper}" /></div>
+        </div>
+    </div>
+    `
+    // var timeleft = 5;
+    // var downloadTimer = setInterval(function(){
+    //     if(timeleft <= 0){
+    //         param.goTo("/instructions/")
+    //         clearInterval(downloadTimer);
+    //     } else ({
+    //         document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+    //     }
+    //     timeleft -= 1;
+    // }, 1000);
+    
+    const imgContainer = div.querySelector(".img-container").children;
+        for (const hand of imgContainer) { 
+            hand.addEventListener("click", () => {  
+             const type = hand.getAttribute("play");
+             // clearInterval(downloadTimer);
+             console.log(type);
+     
+             if (type === "scissors") {
+                state.setMove("scissors");
+                console.log(type);
+             } else if (type === "rock") {
+                state.setMove("rock");
+             } else if (type === "paper") {
+                state.setMove("paper");
+             }
+            });
+        }
+
+    setTimeout(() => {
+        param.goTo("/results");
+    }, 5000);
+
+    const style = document.createElement("style");
     style.innerHTML =`
     .countdown{
         font-family: 'Freckle Face', cursive;
@@ -37,42 +90,7 @@ export function initPlay(param){
           width: 100px:
           height: 100px;
       }
-      
-      
-    `
-    const rock = require("url:../../../../images/piedra.png")
-    const sisors = require("url:../../../../images/tijera.png")
-    const paper = require("url:../../../../images/papel.png")
-
-    div.innerHTML = `
-    <div class = countdown-container>
-        <div class = countdown id="countdown"></div>
-    </div>
-
-    <div class= img-container>
-        <div>
-            <div class = zoom><img class = img src="${rock}" /></div>
-        </div>
-        <div>
-            <div class = zoom><img src="${sisors}" /></div>
-        </div>
-        <div>
-            <div class = zoom><img src="${paper}" /></div>
-        </div>
-    </div>
-    `
-    
-    var timeleft = 5;
-    var downloadTimer = setInterval(function(){
-        if(timeleft <= 0){
-            param.goTo("/results/")
-            clearInterval(timeleft);
-        } else {
-            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-        }
-        timeleft -= 1;
-    }, 1000);
+    ` 
     div.appendChild(style);
-
     return div;
 }
--- src/components/pages/results/index.ts
+++ src/components/pages/results/index.ts
@@ -4,97 +4,95 @@ const resultImages = {
     tie: require("url:../../../../images/piedra.png"),
     win: require("url:../../../../images/win.png"),
     loss: require("url:../../../../images/lose.png"),
-  };
+};
 
 export function initResults(param){
-    const div = document.createElement("div");
-    div.className = "container__results";
+  const div = document.createElement("div");
+  div.className = "container__results";
   
-    const currentState = state.getState();
-    const currentGame = currentState.currentGame;
-    const myPlay = currentGame.myPlay;
-    const computerPlay = currentGame.computerPlay;
-    const whoWins = state.whoWins(myPlay, computerPlay);
+  const currentState = state.getState();
+  const currentGame = currentState.currentGame;
+  const myPlay = currentGame.myPlay;
+  const computerPlay = currentGame.computerPlay;
+  const whoWins = state.whoWins(myPlay, computerPlay);
   
-    //ADD STYLES TO THE BACKGROUND RESULT AND IMG
-    let background;
-    let imagen;
-    if (whoWins === "wins") {
-      imagen = resultImages.win;
-      background = "rgba(136, 137, 73, 0.6)";
-    } else if (whoWins === "loss") {
-      imagen = resultImages.loss;
-      background = "rgba(137, 73, 73, 0.6)";
-    } else {
-      imagen = resultImages.tie;
-      background = "rgba(106, 112, 101, 0.6)";
-    }
+  //ADD STYLES TO THE BACKGROUND RESULT AND IMG
+  let background;
+  let imagen;
+  if (whoWins === "wins") {
+    imagen = resultImages.win;
+    background = "rgba(136, 137, 73, 0.6)";
+  } else if (whoWins === "loss") {
+    imagen = resultImages.loss;
+    background = "rgba(137, 73, 73, 0.6)";
+  } else {
+    imagen = resultImages.tie;
+    background = "rgba(106, 112, 101, 0.6)";
+  }
     
-    const button = require("url:../../../../images/volverAJugar.png");
-    const style = document.createElement("style");
-    div.innerHTML = `
-      <div>
-        <img class="img__result" src="${imagen}">
-      </div>
-      <div class="board">
-        <div>
-          <h3>Score</h3>
-        </div>
-          <text-comp>Vos: ${currentState.history.myScore}</text-comp>
-          <text-comp>Máquina: ${currentState.history.computerScore}</text-comp>
-      </div>
-        <img class = home-button src="${button}">
+  const button = require("url:../../../../images/volverAJugar.png");
+  const style = document.createElement("style");
+  div.innerHTML = `
+    <div>
+      <img class="img__result" src="${imagen}">
     </div>
-    `;
+    <div class="board">
+      <div>
+        <h3>Score</h3>
+      </div>
+        <text-comp>Vos: ${currentState.history.myScore}</text-comp>
+        <text-comp>Máquina: ${currentState.history.computerScore}</text-comp>
+    </div>
+      <img class = home-button src="${button}">
+    </div>
+  `;
 
-    div.querySelector(".home-button").addEventListener("click", () => {
-        param.goTo("./game");
-    });
+  div.querySelector(".home-button").addEventListener("click", () => {
+    param.goTo("./game");
+  });
     
-    // div.querySelector(".back").addEventListener("click", () => {
+    //div.querySelector(".back").addEventListener("click", () => {
     //     param.goTo("/welcome");
     // });
 
-    style.innerHTML = `
+  style.innerHTML = `
     .container__results {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      height: 100vh;
-      justify-content: center;
-      padding-top: 40px;
-      background-color: ${background};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    justify-content: center;
+    padding-top: 40px;
+    background-color: ${background};
     }
     .img__result {
-      height: 180px;
+    height: 180px;
     }
     .board {
-      height: 190px;
-      width: 230px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      border: 10px solid #000;
-      border-radius: 10px;
-      margin: 5px 0;
-      background-color: #fff;
+    height: 190px;
+    width: 230px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border: 10px solid #000;
+    border-radius: 10px;
+    margin: 5px 0;
+    background-color: #fff;
     }
     .board  h3 {
-      text-align: center;
-      font-size: 40px;
-      font-weight: 700;
-      height: 100%;
+    text-align: center;
+    font-size: 40px;
+    font-weight: 700;
+    height: 100%;
     }
     .board > text-comp {
-      align-self: flex-end;
+    align-self: flex-end;
     }
     .back {
-      margin-top: 10px;
+    margin-top: 10px;
     }
   `;
 
   div.appendChild(style);
-
-
-    return div;
+  return div;
 }
